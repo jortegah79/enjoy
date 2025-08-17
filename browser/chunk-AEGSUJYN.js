aa9c9220f29ad5739a14bf58cadf2c0ac240d329
juan ortega <jortegah79@gmail.com>
2025-08-17 18:20:51 +0200

    Auto-generated commit

---
+++ browser/chunk-AEGSUJYN.js
@@ -0,0 +1,1 @@
+import{Da as i,Qb as n}from"./chunk-EEDBMP4G.js";var o=class t{transform(r){if(!r)return"images/no-foto.jpg";let e=r.replaceAll(" ","%20");return`${n.DOMINIO}${e}`}static \u0275fac=function(e){return new(e||t)};static \u0275pipe=i({name:"prepareImage",type:t,pure:!0})};export{o as a};
